ce391729339a7dbec2cdcf56c62922854e6d613c
GraphLib <starkov@cs.karelia.ru>
2014-05-14 01:10:23 +0400

    Update edgelist.hpp
    Вынесено определение класса.
    Добавлена функция конвертации в список смежности.
    Добавлена функция конвертации в матрицу смежности.
    Добавлена перегрузка квадратных скобок.

--- src/edgelist.hpp
+++ src/edgelist.hpp
@@ -11,90 +11,139 @@
 #include <vector>
 #include <algorithm>
 #include "edge.hpp"
-#include "adjlist.hpp"
+#include "data_structures.hpp"
 
-class EdgeList
+EdgeList::EdgeList(std::vector<Edge> edgeList) : edgeList(edgeList)
 {
-    std::vector<Edge> edgeList;
-public:
-    typedef std::vector<Edge>::iterator iterator;
-    int maxVertexNum;
-    EdgeList() {}
-    EdgeList(std::vector<Edge> edgeList) : edgeList(edgeList)
-    {
-        maxVertexNum = -1;
-        for (iterator it = begin(); it != end(); ++it)
-            if (maxVertexNum < std::max(it->u, it->v))
-                maxVertexNum = std::max(it->u, it->v);
-    }
-    /*EdgeList(AdjList g)
-    {
-        unsigned n = g.maxVertexNum;
-        for (unsigned i = 0; i <= n; ++i)
-        {
-            for (AdjList::iterator it = g.begin(i); it != g.end(i); ++it)
-            {
-                edgeList.push_back(Edge(i, it->second, it->first));
-                if (maxVertexNum < std::max(it->second, i))
-                    maxVertexNum = std::max(it->second, i);                  
-            }
-        }
-    }*/
-    ~EdgeList()
-    {
-        edgeList.clear();
-    }
-    void addEdge(Edge edge);
-    void deleteEdge(unsigned FirstVertexNum, unsigned SecondVertexNum);
-    void sort() { std::sort(begin(), end()); }
-    iterator begin() { return edgeList.begin(); }
-    iterator end() { return edgeList.end(); }
-    // Integer overflow possible
-    int cost();
-    friend std::ostream& operator<< (std::ostream& out, EdgeList &list);
-};
-
-void EdgeList::addEdge(Edge edge)
-{
-    edgeList.push_back(edge);
-    if ((int)std::max(edge.u, edge.v) > maxVertexNum)
-        maxVertexNum = std::max(edge.u, edge.v);
+    maxVertexNum = -1;
+    for (int i = 0; i < edgeList.size(); ++i)
+        if (maxVertexNum < std::max(edgeList[i].u, edgeList[i].v))
+            maxVertexNum = std::max(edgeList[i].u, edgeList[i].v);
 }
 
-void EdgeList::deleteEdge(unsigned FirstVertexNum, unsigned SecondVertexNum)
+EdgeList::~EdgeList()
 {
-    int i;
-    if (FirstVertexNum > maxVertexNum || SecondVertexNum > maxVertexNum || FirstVertexNum == SecondVertexNum)
-        throw "This edge not exists!";
-    for (i = 0; i < edgeList.size(); ++i)
-        if (FirstVertexNum == edgeList[i].u && SecondVertexNum == edgeList[i].v)
+    clear();
+}
+
+int EdgeList::addEdge(Edge edge)
+{
+    edgeList.push_back(edge);
+    std::swap(edge.u, edge.v);
+    edgeList.push_back(edge);
+    if (std::max(edge.u, edge.v) > maxVertexNum)
+        maxVertexNum = std::max(edge.u, edge.v);
+    return 0;
+}
+
+int EdgeList::deleteEdge(unsigned u, unsigned v)
+{
+    // Need to recalc maxvertexnum!!!
+    if (u > maxVertexNum || v > maxVertexNum || u == v)
+        return 1;
+    int deleted = 0;
+    for (int i = 0; deleted != 2 && i < edgeList.size(); ++i)
+    {
+        if (std::min(edgeList[i].u, edgeList[i].v) == u &&
+                std::max(edgeList[i].u, edgeList[i].v) == v)
         {
-            for (int j = i + 1; j < edgeList.size(); ++j)
-                edgeList[j - 1] = edgeList[j];
-            break;
+            for (int j = i; j + 1 < edgeList.size(); ++j)
+                edgeList[j] = edgeList[j + 1];
+            edgeList.pop_back();
+            ++deleted;
         }
-    if (i == edgeList.size())
-        throw "This edge not exists!";
-    edgeList.pop_back();
+        else if (std::min(edgeList[i].u, edgeList[i].v) == v &&
+                std::max(edgeList[i].u, edgeList[i].v) == u)
+        {
+            for (int j = i; j + 1 < edgeList.size(); ++j)
+                edgeList[j] = edgeList[j + 1];
+            edgeList.pop_back();
+            ++deleted;
+        }
+    }
+    if (deleted != 2)
+        return 1;
+    return 0;
+}
+
+void EdgeList::clear()
+{
+    edgeList.clear();
+    maxVertexNum = -1;
+}
+
+void EdgeList::toAdjList(AdjList &g)
+{
+    g.resize(maxVertexNum + 1);
+    g.infinity = infinity;
+    g.maxVertexNum = maxVertexNum;
+    for (int i = 0; i < edgeList.size(); ++i)
+        if (edgeList[i].u < edgeList[i].v)
+            g.addEdge(edgeList[i], false);
+}
+
+void EdgeList::toAdjMatrix(AdjMatrix &g)
+{
+    g.resize(maxVertexNum + 1);
+    g.maxVertexNum = maxVertexNum;
+    g.infinity = infinity;
+    for (int i = 0; i <= maxVertexNum; ++i)
+        for (int j = i + 1; j <= maxVertexNum; ++j)
+            g.addEdge(Edge(i, j, infinity));
+    for (int i = 0; i < edgeList.size(); ++i)
+        g.addEdge(edgeList[i]);
+}
+
+int EdgeList::size()
+{
+    return edgeList.size();
+}
+
+void EdgeList::resize(unsigned size)
+{
+    for (int i = (int)edgeList.size() - 1; i > -1; --i)
+    {
+        if (std::max(edgeList[i].u, edgeList[i].v) >= size)
+        {
+            for (int j = i; j + 1 < edgeList.size(); ++j)
+                edgeList[j] = edgeList[j + 1];
+            edgeList.pop_back();
+        }
+    }
+    maxVertexNum = (int)size - 1;
+}
+
+Edge &EdgeList::operator [](int i)
+{
+    return edgeList[i];
 }
 
 int EdgeList::cost()
 {
     int cost = 0;
-    for (iterator it = begin(); it != end(); ++it)
-        cost += it->weight;
+    for (int i = 0; i < edgeList.size(); ++i)
+        cost += edgeList[i].weight;
     return cost;
+}
+
+std::istream& operator>> (std::istream& in, EdgeList &list)
+{
+    int n, m;
+    in >> n >> m;
+    list.resize(n + 1);
+    Edge edge;
+    for (int i = 0; i < m; ++i)
+    {
+        in >> edge;
+        list.addEdge(edge, false);
+    }
+    return in;
 }
 
 std::ostream& operator<< (std::ostream& out, EdgeList &list)
 {
-    unsigned i;
-    for (i = 0; i < list.edgeList.size(); ++i)
-    {
-        out << list.edgeList[i].u << ' ' << list.edgeList[i].v << ' ' << list.edgeList[i].weight << '\n';
-    }
-    if (i != list.edgeList.size())
-        throw "Writing error!";
+    for (int i = 0; i < list.size(); ++i)
+        out << list[i] << '\n';
     return out;
 }
 
