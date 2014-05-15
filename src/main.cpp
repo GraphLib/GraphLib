110e57655170c1dc4a1db02bb4d4fe4f680efdaa
GraphLib <starkov@cs.karelia.ru>
2014-05-16 01:27:41 +0400

    Update main.cpp
    Test for all shortest paths (Floyd and Johnson)

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,42 @@
 #include <stdio.h>
 #include <vector>
 #include <set>
-#include "dijkstra.hpp"
-#include "adjlist.hpp"
-#include "vertexlist.hpp"
+#include "graph.hpp"
+#include "all_shortest_paths.hpp"
 using namespace std;
 
 int main()
 {
-    vector<int> p, d;
-    AdjList g;
-    int n, m;
-    cin >> g;
-    Dijkstra(g, d, p, 1, 1e9);
-    VertexList path = VertexList(p, 1, g.maxVertexNum);
-    if (path.size())
-        cout << path << " ";
-    else cout << "-1 ";
+    Graph g(true);
+    g.addEdge(Edge(0, 1, -1));
+    g.addEdge(Edge(0, 4, 5));
+    g.addEdge(Edge(0, 3, 4));
+    g.addEdge(Edge(1, 2, 2));
+    g.addEdge(Edge(2, 4, 3));
+    g.addEdge(Edge(3, 4, 5));
+    vector< vector<int> > d, p;
+    ParallelFloyd(g, d);
+    cout << "Floyd: " << endl;
+    for (int i = 0; i < d.size(); ++i)
+    {
+        for (int j = 0; j < d[i].size(); ++j)
+            cout << d[i][j] << " ";
+        cout << endl;
+    }
+    cout << "Johnson: " << ParallelJohnson(g, d, p) << endl;
+    for (int i = 0; i < d.size(); ++i)
+    {
+        for (int j = 0; j < d[i].size(); ++j)
+            cout << d[i][j] << " ";
+        cout << endl;
+    }
+    ParallelFloyd(g, d);
+    cout << "Floyd: " << endl;
+    for (int i = 0; i < d.size(); ++i)
+    {
+        for (int j = 0; j < d[i].size(); ++j)
+            cout << d[i][j] << " ";
+        cout << endl;
+    }
     return 0;
 }
