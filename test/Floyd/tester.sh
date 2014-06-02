b30857609fe105ede1e9f32ef6b0ac46c0c904fb
GraphLib <starkov@cs.karelia.ru>
2014-06-02 12:38:28 +0400

    Create tester.sh

---
+++ test/Floyd/tester.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if (($# != 1))
+then
+    printf "usage: <script> <testsCount>"
+    exit 1
+fi
+
+generator=`./GraphGen`
+firstTask=`./BoostFloyd`
+secondTask=`./GraphLibFloyd`
+check=`./checker`
+testsCount=$1
+
+rm -f result.log
+
+for ((i = 1; i <= $testsCount; ++i));
+do
+    echo $generator &> /dev/null
+    echo $firstTask &> /dev/null
+    echo $secondTask &> /dev/null
+    printf "Test $i: " &>> result.log
+    echo $check &>> result.log
+done
