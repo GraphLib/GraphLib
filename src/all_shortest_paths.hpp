647cba8271a3fe1cced2da3c39ad9dc1dc65c2ab
GraphLib <starkov@cs.karelia.ru>
2014-06-24 17:26:42 +0400

    Update all_shortest_paths.hpp

--- src/all_shortest_paths.hpp
+++ src/all_shortest_paths.hpp
@@ -9,6 +9,7 @@
 #define	ALL_SHORTEST_PATHS_HPP
 
 #include <assert.h>
+#include <omp.h>
 #include "graph.hpp"
 #include "shortest_paths.hpp"
 
@@ -24,7 +25,6 @@ void ParallelFloyd(Graph& g, std::vector< std::vector< int > >& distance)
     distance.resize(g.verticesCount());
     for (int i = 0, n = g.verticesCount(); i < n; ++i)
         distance[i].assign(g.adjMatrix[i].begin(), g.adjMatrix[i].end());
-    //omp_set_num_threads(omp_get_num_threads());
     omp_set_num_threads(8);
     for (int k = 0; k < distance.size(); ++k)
     #pragma omp parallel for private (t1, t2)
@@ -46,7 +46,7 @@ void ParallelFloyd(Graph& g, std::vector< std::vector< int > >& distance)
 bool ParallelJohnson(Graph &g, std::vector< std::vector< int > >& distance,
         std::vector< std::vector<int> >& predecessor)
 {
-    g.resize(g.verticesCount() + 1);
+   g.resize(g.verticesCount() + 1);
     for (unsigned i = 0, n = (unsigned)g.verticesCount() - 1; i < n; ++i)
         g.addEdge(Edge(n, i, 0));
     std::vector<int> h;
@@ -54,25 +54,25 @@ bool ParallelJohnson(Graph &g, std::vector< std::vector< int > >& distance,
         return false;
     for (int i = 0; i < g.edgeList.size(); ++i)
         g.edgeList[i].weight += h[g.edgeList[i].u] - h[g.edgeList[i].v];
-    //omp_set_num_threads(omp_get_num_threads())
-    //#pragma omp parallel for
+    omp_set_num_threads(8);
     distance.resize(g.verticesCount() - 1);
     predecessor.resize(g.verticesCount() - 1);
     g.toAdjList();
-    for (unsigned i = 0, n = (unsigned)g.verticesCount() - 1; i < n; ++i)
+    unsigned n = (unsigned)g.verticesCount() - 1;
+    for (unsigned i = 0; i < n; ++i)
         g.deleteEdge(n, i);
     g.resize(g.verticesCount() - 1);
-    for (unsigned i = 0, n = (unsigned)g.verticesCount(); i < n; ++i)
+    #pragma omp parallel for
+    for (int i = 0; i < n; ++i)
     {
         Dijkstra(g, distance[i], predecessor[i], i);
         for (unsigned j = 0; j < n; ++j)
             if (distance[i][j] != g.infinity)
                 distance[i][j] += h[j] - h[i];
     }
-    for (unsigned i = 0, n = (unsigned)g.verticesCount(); i < n; ++i)
+    for (unsigned i = 0; i < n; ++i)
         for (int j = 0; j < g.adjList[i].size(); ++j)
             g.adjList[i][j].first -= h[i] - h[g.adjList[i][j].second];
     return true;
 }
-
 #endif	/* ALL_SHORTEST_PATHS_HPP */
